226d40864d8d98bc7d150290bd207c28e507d293
hex7c0 <carniellifrancesco@gmail.com>
2014-05-15 13:33:39 +0200

    console

--- console/console.js
+++ console/console.js
@@ -1,6 +1,5 @@
 "use strict";
 // variables
-var clock = 2000;
 var avg = null;
 var meml = null;
 var memp = null;
@@ -131,107 +130,6 @@ function load() {
     });
     return;
 }
-function dyna($http, $scope, $timeout) {
-    /**
-     * ajax post 'dynamic'
-     */
-
-    function loop() {
-        $http({
-            method : 'POST',
-            url : '/dyn/'
-        }).success(
-                function(data, status, headers, config) {
-                    // avg
-                    store.x.push(new Date(data.date).toUTCString());
-                    store.one.push(data.cpu.one);
-                    store.five.push(data.cpu.five);
-                    store.fifteen.push(data.cpu.fifteen);
-                    // mem
-                    store.total.push(data.mem.total);
-                    store.used.push(data.mem.used);
-                    store.free.push(data.mem.free);
-                    store.v8rss.push(data.mem.v8.rss);
-                    store.v8total.push(data.mem.v8.total);
-                    store.v8used.push(data.mem.v8.used);
-                    store.v8free.push(data.mem.v8.free);
-                    // chart
-                    avg.load({
-                        columns : [ store.x, store.one, store.five,
-                                store.fifteen ],
-                    });
-                    meml.load({
-                        columns : [ store.x, store.total, store.used,
-                                store.v8rss, store.v8total, store.v8used, ],
-                    });
-                    memp.load({
-                        columns : [ [ 'used', data.mem.used ],
-                                [ 'free', data.mem.free ] ],
-                    });
-                    // info
-                    $scope.dynamics = [ {
-                        title : 'System lag',
-                        info : data.ns + ' nanoseconds,'
-                    }, {
-                        title : 'System uptime',
-                        info : Math.floor(data.uptime / 60) + ' minute',
-                    }, ];
-                    dyna($http, $scope, $timeout)
-                }).error(function(data, status, headers, config) {
-            alert('server donesn\'t respond');
-        });
-    }
-    if ($scope.clock > 0) {
-        $timeout(loop, $scope.clock);
-    }
-    return;
-}
-function stat($http, $scope) {
-    /**
-     * ajax post 'static'
-     */
-
-    $http({
-        method : 'POST',
-        url : '/sta/'
-    }).success(function(data, status, headers, config) {
-        // info
-        $scope.statics = [ {
-            title : 'OS hostname',
-            info : data.os.hostname
-        }, {
-            title : 'OS platform',
-            info : data.os.platform
-        }, {
-            title : 'CPU architecture',
-            info : data.os.arch
-        }, {
-            title : 'OS type',
-            info : data.os.type
-        }, {
-            title : 'OS release',
-            info : data.os.release
-        }, {
-            title : 'Node version',
-            info : data.version.node
-        }, {
-            title : 'Module versions',
-            info : data.version.module
-        }, {
-            title : 'Process gid',
-            info : data.process.gid
-        }, {
-            title : 'Process uid',
-            info : data.process.uid
-        }, {
-            title : 'Process pid',
-            info : data.process.pid
-        }, ];
-    }).error(function(data, status, headers, config) {
-        alert('server donesn\'t respond');
-    });
-    return;
-}
 // init
 load();
 app.controller('static', function($scope, $http, $timeout) {
@@ -239,7 +137,7 @@ app.controller('static', function($scope, $http, $timeout) {
     $scope.clock = 1;
     stat($http, $scope);
     dyna($http, $scope, $timeout);
-    $scope.clock = clock;
+    $scope.clock = 2;
     // click
     $scope.data.refresh = function(item, event) {
         if (item == 'dynamic') {
