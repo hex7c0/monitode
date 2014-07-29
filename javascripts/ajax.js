84048d487626901013af65102af7efaa0aed7f9c
hex7c0 <carniellifrancesco@gmail.com>
2014-07-29 12:58:54 +0200

    update pages

--- javascripts/ajax.js
+++ javascripts/ajax.js
@@ -1,10 +1,13 @@
-var promise = null;
-function dyna($http, $scope, $timeout) {
-    /**
-     * ajax post 'dynamic'
-     */
+var promise;
+function dyna($http,$scope,$timeout) {
 
     $timeout.cancel(promise);
+    if ($scope.clock > 0) {
+        promise = $timeout(loop,$scope.clock * 1000);
+    } else if ($scope.clock == 0) {
+        $scope.clock = 5;
+        loop();
+    }
 
     function loop() {
 
@@ -13,138 +16,132 @@ function dyna($http, $scope, $timeout) {
         store.one.push(Math.random());
         store.five.push(Math.random());
         store.fifteen.push(Math.random());
+
         // mem
         var testing = 1073741824;
         store.total.push(testing);
         store.used.push(testing / 4);
-        store.free.push(testing - (testing / 4));
         store.v8rss.push(testing / 6);
         store.v8total.push(testing / 8);
         store.v8used.push(testing / 10);
-        store.v8free.push((testing / 8) - (testing / 10));
+
         // chart
+        var X = store.x;
         avg.load({
-            columns : [ store.x, store.one, store.five, store.fifteen ],
+            columns: [X,store.one,store.five,store.fifteen],
         });
         meml.load({
-            columns : [ store.x, store.total, store.used, store.v8rss,
-                    store.v8total, store.v8used, ],
+            columns: [X,store.total,store.used,store.v8rss,store.v8total,
+                    store.v8used],
         });
         memp.load({
-            columns : [ [ 'used', testing / 4 ],
-                    [ 'free', testing - (testing / 4) ] ],
+            columns: [['used',testing / 4],['free',testing - (testing / 4)]],
         });
+
         // cpu
         if (cpus.length == 0) {
-            $scope.cpus = cpus = [ {
-                times : {
-                    user : Math.random(),
-                    nice : 0,
-                    sys : Math.random(),
-                    idle : Math.random(),
-                    irq : 0
+            $scope.cpus = cpus = [{
+                times: {
+                    user: Math.random(),
+                    nice: 0,
+                    sys: Math.random(),
+                    idle: Math.random(),
+                    irq: 0
                 }
-            }, {
-
-                times : {
-                    user : Math.random(),
-                    nice : 0,
-                    sys : Math.random(),
-                    idle : Math.random(),
-                    irq : 0
+            },{
+                times: {
+                    user: Math.random(),
+                    nice: 0,
+                    sys: Math.random(),
+                    idle: Math.random(),
+                    irq: 0
                 }
-            }, {
-
-                times : {
-                    user : Math.random(),
-                    nice : 0,
-                    sys : Math.random(),
-                    idle : Math.random(),
-                    irq : 0
+            },{
+                times: {
+                    user: Math.random(),
+                    nice: 0,
+                    sys: Math.random(),
+                    idle: Math.random(),
+                    irq: 0
                 }
-            }, {
-                times : {
-                    user : Math.random(),
-                    nice : 0,
-                    sys : Math.random(),
-                    idle : Math.random(),
-                    irq : 0
+            },{
+                times: {
+                    user: Math.random(),
+                    nice: 0,
+                    sys: Math.random(),
+                    idle: Math.random(),
+                    irq: 0
                 }
-            } ];
-            proc($scope.cpus);
+            }];
+            loadProc($scope.cpus);
         } else {
-            for (var i = 0; i < cpus.length; i++) {
+            for (var i = 0, xx = cpus.length; i < xx; i++) {
                 cpus[i].load({
-                    columns : [ [ 'user', Math.random() ], [ 'nice', 0 ],
-                            [ 'sys', Math.random() ],
-                            [ 'idle', Math.random() ], [ 'irq', 0 ] ],
+                    columns: [['user',Math.random()],['nice',0],
+                            ['sys',Math.random()],['idle',Math.random()],
+                            ['irq',0]],
                 });
             }
         }
-        // info
-        $scope.dynamics = [ {
-            title : 'Ajax lag',
-            info : 0 + ' milliseconds',
-        }, {
-            title : 'System lag',
-            info : Math.random() + ' nanoseconds',
-        }, {
-            title : 'System uptime',
-            info : 0 + ' minutes',
-        }, {
-            title : 'System uptime Node',
-            info : 0 + ' minutes',
-        }, ];
-        return dyna($http, $scope, $timeout)
+
+        $scope.dynamics = [{
+            title: 'Ajax lag',
+            info: 0 + ' milliseconds',
+        },{
+            title: 'System lag',
+            info: Math.random() + ' nanoseconds',
+        },{
+            title: 'System uptime',
+            info: 0 + ' minutes',
+        },{
+            title: 'System uptime Node',
+            info: 0 + ' minutes',
+        },];
+        return dyna($http,$scope,$timeout)
     }
 
-    if ($scope.clock > 0) {
-        promise = $timeout(loop, $scope.clock * 1000);
-    } else if ($scope.clock == 0) {
-        $scope.clock = 2;
-        loop();
-    }
     return;
 }
-function stat($http, $scope) {
-    /**
-     * ajax post 'static'
-     */
+
+function stat($http,$scope) {
 
     // info
-    $scope.statics = [ {
-        title : 'CPU architecture',
-        info : 'testing page'
-    }, {
-        title : 'OS hostname',
-        info : 'testing page'
-    }, {
-        title : 'OS platform',
-        info : 'testing page'
-    }, {
-        title : 'OS type',
-        info : 'testing page'
-    }, {
-        title : 'OS release',
-        info : 'testing page'
-    }, {
-        title : 'Process gid',
-        info : 'testing page'
-    }, {
-        title : 'Process uid',
-        info : 'testing page'
-    }, {
-        title : 'Process pid',
-        info : 'testing page'
-    }, {
-        title : 'Process env',
-        info : 'testing page'
-    }, {
-        title : 'Network interfaces',
-        info : 'testing page'
-    }, {
-        title : 'Node versions',
-        info : 'testing page'
-    }, ];
+    $scope.statics = [{
+        title: 'CPU architecture',
+        info: 'testing page'
+    },{
+        title: 'OS hostname',
+        info: 'testing page'
+    },{
+        title: 'OS platform',
+        info: 'testing page'
+    },{
+        title: 'OS type',
+        info: 'testing page'
+    },{
+        title: 'OS release',
+        info: 'testing page'
+    },{
+        title: 'OS endianness',
+        info: 'testing page'
+    },{
+        title: 'Process gid',
+        info: 'testing page'
+    },{
+        title: 'Process uid',
+        info: 'testing page'
+    },{
+        title: 'Process pid',
+        info: 'testing page'
+    },{
+        title: 'Process environment',
+        info: 'testing page'
+    },{
+        title: 'Network interfaces',
+        info: 'testing page'
+    },{
+        title: 'Node versions',
+        info: 'testing page'
+    }];
     return;
 }
